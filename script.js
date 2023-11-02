fa78563a192f5ed709ab70a361fe5ad299c54b3c
Nilam <nilammartaanggreni@gmail.com>
2023-11-02 18:25:18 +0800

    responsive design 4

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   // untuk menambahkan class nav-open pada bagian header agar dapat memperlihatkan menu navigasi jika nav tidak ada
   headerEl.classList.toggle("nav-open");
+
+  // const href = link.getAttribute("href");
+  // console.log(href);
+  // if (href === "#") {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: "smooth",
+  //   });
+  // }
 });
---
+++ slider.js
@@ -0,0 +1,8 @@
+new Splide(".splide").mount();
+
+var splide = new Splide(".splide", {
+  type: "loop",
+  perPage: 1,
+});
+
+splide.mount();
