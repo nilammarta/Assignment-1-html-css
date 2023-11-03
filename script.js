a38217c945bfe0dad68a05bbd2048f3469d3d06c
Nilam <nilammartaanggreni@gmail.com>
2023-11-03 18:15:37 +0800

    responsive Design part 5

--- script.js
+++ script.js
@@ -6,13 +6,32 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   // untuk menambahkan class nav-open pada bagian header agar dapat memperlihatkan menu navigasi jika nav tidak ada
   headerEl.classList.toggle("nav-open");
-
-  // const href = link.getAttribute("href");
-  // console.log(href);
-  // if (href === "#") {
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth",
-  //   });
-  // }
 });
+
+const sectionHeroEL = document.querySelector(".header");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    // lakukan pengecekan apabila halaman sudah memasuki section SELAIN hero-section maka tambahkan sticky nya
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    // jika sudah berada pada hero-section hapus bagian sticky nya
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the view port
+    root: null,
+    // untuk memulai dari mana elemen akan muncul
+    threshold: 0,
+    // bagian ini untuk memulai menu nav pada bagian akhir dari hero section sesuai dengan ukuran height sticky nya
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEL);
