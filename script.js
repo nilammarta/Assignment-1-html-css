f642390f9a10bd21314385156f2efbe326143a3e
Nilam <nilammartaanggreni@gmail.com>
2023-11-06 14:38:57 +0800

    responsive design 6

--- script.js
+++ script.js
@@ -8,30 +8,30 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-const sectionHeroEL = document.querySelector(".header");
+// const sectionHeroEL = document.querySelector(".header");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
 
-    // lakukan pengecekan apabila halaman sudah memasuki section SELAIN hero-section maka tambahkan sticky nya
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
+//     // lakukan pengecekan apabila halaman sudah memasuki section SELAIN hero-section maka tambahkan sticky nya
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
 
-    // jika sudah berada pada hero-section hapus bagian sticky nya
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // in the view port
-    root: null,
-    // untuk memulai dari mana elemen akan muncul
-    threshold: 0,
-    // bagian ini untuk memulai menu nav pada bagian akhir dari hero section sesuai dengan ukuran height sticky nya
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEL);
+//     // jika sudah berada pada hero-section hapus bagian sticky nya
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // in the view port
+//     root: null,
+//     // untuk memulai dari mana elemen akan muncul
+//     threshold: 0,
+//     // bagian ini untuk memulai menu nav pada bagian akhir dari hero section sesuai dengan ukuran height sticky nya
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionHeroEL);
